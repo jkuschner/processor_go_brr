5fa0f3353d0c7063be7d39bab5d7c7c85a66bfa1
jkuschner <60696513+jkuschner@users.noreply.github.com>
2022-02-08 16:17:17 -0800

    updated Definitions.sv to fit our ISA

--- sv/Definitions.sv
+++ sv/Definitions.sv
@@ -5,23 +5,19 @@
 //   packages very useful for declaring global variables
 package definitions;
     
-/*
-    Replace AND with OR
-    Add an RXR ALU operation.
-
-*/
 // Instruction map
     const logic [2:0]kADD  = 3'b000;
     const logic [2:0]kLSH  = 3'b001;
     const logic [2:0]kRSH  = 3'b010;
     const logic [2:0]kXOR  = 3'b011;
-    const logic [2:0]kAND  = 3'b100;
+    const logic [2:0]kORR  = 3'b100;
 	const logic [2:0]kSUB  = 3'b101;
 	const logic [2:0]kCLR  = 3'b110;
+	const logic [2:0]kRXR  = 3'b111;
 // enum names will appear in timing diagram
     typedef enum logic[2:0] {
         ADD, LSH, RSH, XOR,
-        AND, SUB, CLR } op_mne;
+        ORR, SUB, CLR, RXR } op_mne;
 // note: kADD is of type logic[2:0] (3-bit binary)
 //   ADD is of type enum -- equiv., but watch casting
 //   see ALU.sv for how to handle this   
