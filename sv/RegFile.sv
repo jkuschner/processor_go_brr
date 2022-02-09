13a7ee37347069a9b56a422b4f6d910495bf3de9
cher-ban <42155195+cher-ban@users.noreply.github.com>
2022-02-08 21:01:56 -0800

    Update RegFile.sv

--- sv/RegFile.sv
+++ sv/RegFile.sv
@@ -1,12 +1,5 @@
-// Create Date:    2019.01.25
-// Design Name:    CSE141L
-// Module Name:    reg_file 
-//
-// Additional Comments: 					  $clog2
 
-/* parameters are compile time directives 
-       this can be an any-width, any-depth reg_file: just override the params!
-*/
+// Module Name:    reg_file 
 
 /*
 Inputs: RaddrA/RaddrB/Waddr(from decoder)
@@ -14,40 +7,34 @@ Inputs: RaddrA/RaddrB/Waddr(from decoder)
         DataIn(from ALU, from data_memory, from LUT)
 
 Outputs: DataOutA/DataOutB(to ALU, to LUT, to data_memory, to Program Counter)
-
 */
 module RegFile #(parameter W=8, A=4)(		 // W = data path width (leave at 8); A = address pointer width
   input                Clk,
                        Reset,
                        WriteEn,
-  input        [A-1:0] RaddrA,				 // address pointers
+  input        [2:0]   RaddrA,				 // address pointers
                        RaddrB,
                        Waddr,
   input        [W-1:0] DataIn,
-  output       [W-1:0] DataOutA,			 // showing two different ways to handle DataOutX, for
-  output logic [W-1:0] DataOutB				 //   pedagogic reasons only
+  output       [W-1:0] DataOutA,			 
+  output logic [W-1:0] DataOutB				 
     );
 
 // W bits wide [W-1:0] and 2**4 registers deep 	 
 logic [W-1:0] Registers[2**A];	             // or just registers[16] if we know A=4 always
 
 // combinational reads 
-/* can write always_comb in place of assign
-    difference: assign is limited to one line of code, so
-	always_comb is much more versatile     
-*/
-assign      DataOutA = Registers[RaddrA];	 // 
-always_comb DataOutB = Registers[RaddrB];    // can read from addr 0, just like ARM
+always_comb begin
+    DataOutA = Registers[RaddrA];	 
+    DataOutB = Registers[RaddrB];    
+end
 
 // sequential (clocked) writes 
 always_ff @ (posedge Clk) begin
   if (Reset) begin
-    Registers[0] <= '0;
-    Registers[1] <= '0;
-    Registers[2] <= '0;
-    Registers[3] <= '0;
-    Registers[4] <= '0;
-  end else if (WriteEn) begin	                             // works just like data_memory writes
+    for(int i = 0; i < 16; i++)
+	    Registers[i] <= 0;
+  end else if (WriteEn) begin	                  
     Registers[Waddr] <= DataIn;
   end
 end
