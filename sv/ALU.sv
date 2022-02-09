cdbdcdba35e1229e772188b6285475fa53403662
Nataly Buhr <60751033+nbuhr9@users.noreply.github.com>
2022-02-08 18:31:40 -0800

    Added RXR and ORR, redefined LSH and RSH

--- sv/ALU.sv
+++ sv/ALU.sv
@@ -10,10 +10,10 @@ import definitions::*;			          // includes package "definitions"
 /*
 Inputs: InputA/InputB(from reg file)
         OP(from decoder)
-        NOTE: Possibly require another input for register 8
+  NOTE: Possibly require another input for register 8? Doesn't seem necessary yet, depends on top level
 
 Outputs: Out(goes to reg_file)
-        Zero(goes to Prog_Ctr)
+         Zero(goes to Prog_Ctr)
 
 For shift operations, InputA is the data to be shifted, InputB is the shift amount.
 Need to add an RXR ALU op and set signals accordingly.
@@ -21,12 +21,12 @@ Need to add an RXR ALU op and set signals accordingly.
 module ALU #(parameter W=8, Ops=3)(
   input        [W-1:0]   InputA,          // data inputs
                          InputB,
-  input        [Ops-1:0] OP,		      // ALU opcode, part of microcode
+  input        [Ops-1:0] OP,		  // ALU opcode, part of microcode
   input                  SC_in,           // shift or carry in
-  output logic [W-1:0]   Out,		      // data output 
+  output logic [W-1:0]   Out,		  // data output 
   output logic           Zero,            // output = zero flag	 !(Out)
                          Parity,          // outparity flag  ^(Out)
-                         Odd			  // output odd flag (Out[0])
+                         Odd		  // output odd flag (Out[0])
 // you may provide additional status flags, if desired
     );								    
 	 
@@ -36,18 +36,19 @@ module ALU #(parameter W=8, Ops=3)(
     Out = 0;                              // No Op = default
     case(OP)							  
       ADD : Out = InputA + InputB;        // add 
-      LSH : Out = {InputA[6:0],SC_in};    // shift left, fill in with SC_in 
-// for logical left shift, tie SC_in = 0
-	  RSH : Out = {1'b0, InputA[7:1]};    // shift right
- 	  XOR : Out = InputA ^ InputB;        // bitwise exclusive OR
+      LSH : Out = InputA << InputB;       // logical shift left (InputB is grabbed from r8)
+      RSH : Out = InputA >> InputB;       // logical shift right (InputB is grabbed from r8)
+      XOR : Out = InputA ^ InputB;        // bitwise XOR
+      RXR : Out = ^InputA;                // reduction XOR
       AND : Out = InputA & InputB;        // bitwise AND
-      SUB : Out = InputA + (~InputB) + 1;
+      SUB : Out = InputA + (~InputB) + 1; // sub
+      ORR : Out = InputA | InputB;        // bitwise OR
     endcase
   end
 
   assign Zero   = !Out;                   // reduction NOR
   assign Parity = ^Out;                   // reduction XOR
-  assign Odd    = Out[0];				  // odd/even -- just the value of the LSB
+  assign Odd    = Out[0];		  // odd/even -- just the value of the LSB
 
   always_comb
     op_mnemonic = op_mne'(OP);			  // displays operation name in waveform viewer
