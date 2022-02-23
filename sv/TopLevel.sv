0811f02c07813657e1daf09602805db36f561fbc
Joshua <jkuschne@ucsd.edu>
2022-02-23 00:42:21 -0800

    Updated all wire initialization to match diagram w/ comments

--- sv/TopLevel.sv
+++ sv/TopLevel.sv
@@ -4,28 +4,36 @@
 // CSE141L
 // partial only										   
 module TopLevel(		   // you will have the same 3 ports
-    input     Reset,	   // init/reset, active high
-			     Start,    // start next program
-	             Clk,	   // clock -- posedge used inside design
+    input     	Reset,	   // init/reset, active high
+			    Start,     // start next program
+	            Clk,	   // clock -- posedge used inside design
     output logic Ack	   // done flag from DUT
     );
 
-wire [ 9:0] PgmCtr,        // program counter
-			PCTarg;
-wire [ 8:0] Instruction;   // our 9-bit opcode
-wire [ 7:0] ReadA, ReadB;  // reg_file outputs
-wire [ 7:0] InA, InB, 	   // ALU operand inputs
-            ALU_out;       // ALU result
-wire [ 7:0] RegWriteValue, // data in to reg file
-            MemWriteValue, // data in to data_memory
-	    MemReadValue;  // data out from data_memory
-wire        MemWrite,	   // data_memory write enable
-	    RegWrEn,	   // reg_file write enable
-	    Zero,          // ALU output = 0 flag
-            Jump,	   // to program counter: jump 
-            BranchEn;	   // to program counter: branch enable
-wire [1:0]  TargSel;
-logic[15:0] CycleCt;	   // standalone; NOT PC!
+wire [ 9:0] PgmCtr;        	// PC -> Inst_ROM
+wire [ 1:0] PCRegAddr;		// decoder -> PC
+wire 		JumpEq,			// decoder -> PC
+			JumpNotEq,		// decoder -> PC
+			OffsetEn;		// decoder -> PC
+wire [ 8:0] Instruction;   	// our 9-bit opcode, Inst_ROM -> Ctrl
+wire [ 7:0] RegOutA, 		// RegFile -> ALU, DataMem, LUT_LSW
+			RegOutB;  		// RegFile -> ALU, DataMem, LUT_MSW, PC
+wire [ 1:0] Reg3IndexOut;	// RegFile -> Ctrl
+wire [ 7:0] RegWriteValue, 	// data in to reg file, 8:1 mux -> RegFile
+	    	DataMemOut,  	// data out from data_memory, DataMem -> 8:1 mux
+			ALU_out,       	// ALU -> 8:1 mux
+			bFlip_MSW,		// LUT_MSW -> 8:1 mux
+			bFlip_LSW,		// LUT_LSW -> 8:1 mux
+			ImmOut;			// Ctrl -> 8:1 mux
+wire [ 2:0] ReadRegAddrA,	// Ctrl -> RegFile
+			ReadRegAddrB,	// Ctrl -> RegFile
+			WriteRegAddr,	// Ctrl -> RegFile
+			WriteSource;	// Ctrl -> 8:1 mux (only 5 used)
+wire [ 3:0] ALUOp;			// Ctrl -> ALU
+wire        MemWrite,		// data_memory write enable, Ctrl -> dataMem
+	    	RegWrEn,	   	// reg_file write enable, Ctrl -> RegFile
+	    	Zero,          	// ALU output = 0 flag, ALU -> PC
+logic[15:0] CycleCt;	   	// standalone; NOT PC!
 
 // Fetch stage = Program Counter + Instruction ROM
   ProgCtr PC1 (		       // this is the program counter module
